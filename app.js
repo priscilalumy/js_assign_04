08198c34a7d95643efc3d4266393a9e8ddecff1f
priscilalumy <priscilalumyca@gmail.com>
2017-03-21 05:42:54 +0000

    third commit

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// code from https://code-maven.com/todo-in-html-and-javascript
+
 function get_todos() {
     var todos = new Array;
     var todos_str = localStorage.getItem('todo');
@@ -6,16 +8,16 @@ function get_todos() {
     }
     return todos;
 }
- 
+
 function add() {
     var task = document.getElementById('addTodoItem').value;
- 
+
     var todos = get_todos();
     todos.push(task);
     localStorage.setItem('todo', JSON.stringify(todos));
- 
+
     show();
- 
+
     return false;
 }
 
@@ -24,28 +26,28 @@ function remove() {
     var todos = get_todos();
     todos.splice(id, 1);
     localStorage.setItem('todo', JSON.stringify(todos));
- 
+
     show();
- 
+
     return false;
 }
- 
+
 function show() {
     var todos = get_todos();
- 
+
     var html = '<ul>';
     for(var i=0; i<todos.length; i++) {
         html += '<li>' + todos[i] + '<button class="remove" id="' + i  + '">delete</button></li>';
     };
     html += '</ul>';
- 
+
     document.getElementById('todoList').innerHTML = html;
- 
+
     var buttons = document.getElementsByClassName('remove');
     for (var i=0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', remove);
     };
 }
- 
+
 document.getElementById('addTodo').addEventListener('click', add);
 show();
